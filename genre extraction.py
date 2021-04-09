80f7246bd6be78820ae1ff5f0798cd6008290d3a
wyuan4 <wen_yuan@brown.edu>
2021-04-08 21:45:39 -0500

    Create genre extraction.py
    added a new python file that loops through the genre column of the Netflix titles dataset and creates a unique list of genres: Ashley coding, Erik debugging (1.5 hours)

---
+++ genre extraction.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+df = pd.read_csv (r'C:\Users\yuanm\Documents\School Documents\CLPS 0950\Python\CLPS0950FinalProject\CLPS0950FinalProject\netflix_titles.csv')
+#print(df)
+
+#loop through pandas DataFrame "listedin" column of genres and add genres
+#to netflix_genres list
+netflix_genres = []
+for x in range(0,7787):
+    netflix_genres.append(df.iat[x,10])
+    
+#creates a list of all the unique genres from the Netflix titles dataset
+category_list = []
+for x in range(0,len(netflix_genres)):
+    text = netflix_genres[x]
+    phrases = text.split(",")
+    for y in range(0,len(phrases)):
+        phrases[y] = phrases[y].lstrip()
+        if phrases[y] not in category_list:
+            category_list.append(phrases[y])
+
+print(category_list)
+
+
