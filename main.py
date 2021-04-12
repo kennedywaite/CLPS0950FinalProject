f6d5ee87c25672a5f4a07250070b18ebd2dabf48
wyuan4 <wen_yuan@brown.edu>
2021-04-11 20:23:42 -0500

    Update main.py
    changed the variable names to be more broad because the functionality of category_extraction was broadened -- Ashley typing (15 minutes)

--- main.py
+++ main.py
@@ -3,26 +3,25 @@
 import pandas as pd
 
 df = pd.read_csv (r'https://raw.githubusercontent.com/kennedywaite/CLPS0950FinalProject/main/netflix_titles.csv')
-print(df)
 
 #this function makes a list of all the different unique types of a category
 #listed in
 def category_extraction(dataF, col_num):
-#loop through pandas DataFrame "listedin" column of genres and add genres
-#to netflix_genres list
-    netflix_genres = []
+#loop through pandas DataFrame column "col_num" and make a list of all entries
+    list_items = []
     for x in range(0,7787):
-        netflix_genres.append(dataF.iat[x,col_num])
+        list_items.append(dataF.iat[x,col_num])
     
-#creates a list of all the unique genres from the Netflix titles dataset
+#creates a list of all the unique items from list of entries in col_num column
     category_list = []
-    for x in range(0,len(netflix_genres)):
-        text = netflix_genres[x]
-        phrases = text.split(",")
-        for y in range(0,len(phrases)):
-            phrases[y] = phrases[y].lstrip()
-            if phrases[y] not in category_list:
-                category_list.append(phrases[y])
+    for x in range(0,len(list_items)):
+        if (list_items[x] != ''):
+            text = list_items[x]
+            phrases = text.split(",")
+            for y in range(0,len(phrases)):
+                phrases[y] = phrases[y].lstrip()
+                if phrases[y] not in category_list:
+                    category_list.append(phrases[y])
 
     return(category_list)
 
@@ -35,3 +34,5 @@ release_col = 7
 duration_col = 9
 
 genre_list = category_extraction(df,genres_col)
+director_list = category_extraction(df,director_col)
+print(director_list)
