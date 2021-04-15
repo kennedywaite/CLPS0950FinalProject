9276ab116d3febfd2fe992e3c21c2336e5d9eb58
wyuan4 <wen_yuan@brown.edu>
2021-04-14 19:23:28 -0500

    Update user_input.py
    slight change adding row to matrix if input is yes -- Ashley typing (10 minutes)

--- user_input.py
+++ user_input.py
@@ -1,15 +1,17 @@
 #trying to test out creating new users and doing similarity indexes between
 #users to make an algorithm for a Netflix recommendation engine
+import numpy as np
 
-user_matrix = [for i in range(7787)]
+user_matrix = np.zeros((4,7787))
 print(user_matrix)
 
 new_user = input("Are you a new user? [Yes/No]")
 
-#if (new_user == "Yes"):
+if (new_user == "Yes"):
     #add new row to user_matrix
-    
-#elif (new_user == "No"):
+    user_matrix.append((np.zeros(1,7787)))
+elif (new_user == "No"):
+    user_matrix.append(1)
     #ask for username
-#else: 
-#    new_user = input("Are you a new user? [Yes/No]")
+else: 
+    new_user = input("Are you a new user? [Yes/No]")
