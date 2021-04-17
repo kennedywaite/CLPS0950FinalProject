7ff350e562cc5311c87ece86fba0d5c596990333
wyuan4 <wen_yuan@brown.edu>
2021-04-16 20:24:06 -0500

    Update button_test.py
    deleted commented code that we aren't going to use -- Ashley typing (5 min)

--- button_test.py
+++ button_test.py
@@ -9,7 +9,6 @@ class Application(tk.Frame):
         self.master = master
         self.pack(fill=tk.BOTH,expand=True)
         counter=0
-        #master.geometry("300x500")
         self.create_quit()
         for x in ran_list:
             self.create_genre_widgets(str(x), counter, button_identities)
@@ -24,25 +23,14 @@ class Application(tk.Frame):
             text = df.iat[x,genres_col]
             if user_input in text:
                 output_list.append(df.iat[x,2])
-        #print(output_list)
-        #self.button.configure(bg="red")
-        
-        
             
         button_name = (button_identities[counter])
         button_name.configure(bg="red")
-        
-    #def update_list(self, output_list, current_list):
-        #if current list is empty then equal to output list
-        #else only keep same elements between lists
-        
-        #return current_list
-        
+                
     def create_inc(self, button_identity):
         self.button = tk.Button(self,text="Inclusive",fg="blue",command =lambda: self.inc_clicked(button_identity))
         
         self.button.grid(row = 8, column=2, pady=100)
-        
         
     def inc_clicked(self, but_id):
         new_list = []
@@ -59,8 +47,6 @@ class Application(tk.Frame):
                 text = df.iat[y,genres_col]
                 if (x in text) and (df.iat[y,2] not in output_list):
                     output_list.append(df.iat[y,2])
-                    
-        #print(output_list)
         self.show_titles(output_list)
         
     def create_exc(self, button_identity):
@@ -82,23 +68,15 @@ class Application(tk.Frame):
             text = df.iat[y,genres_col]
             if all(x in text for x in new_list):
                 output_list.append(df.iat[y,2])
-                    
-        #print(output_list)
         self.show_titles(output_list)
     
 
     def create_genre_widgets(self, m_title, counter, button_identities):
-        
         self.button = tk.Button(self,text=str(m_title),command =lambda: self.genre_clicked(m_title,button_identities,counter))
-        #self.button["text"] = str(m_title)
-      
-            
         self.button.grid(row= counter%6, column=counter%7)
-            
         button_identities.append(self.button)
                     
     def create_quit(self):
-     
         self.quit = tk.Button(self, text="QUIT", fg="red",
                               command=self.master.destroy)
         self.quit.grid(row = 8, column = 4, pady=100)
@@ -108,12 +86,8 @@ class Application(tk.Frame):
         newWindow.title("10 Random Movies/TV Shows of Selected Items")
         new_counter = 0
         new_button_identities = []
-        
         tk.Label(newWindow,text="Here are your shows").grid(row=0,column=0,pady=10)
-        
         rand_list = random.sample(shows_list,10)
-        #print(rand_list)
-        
         for x in rand_list:
             self.create_show_titles(newWindow,x,new_counter,new_button_identities)
             new_counter +=1
@@ -126,7 +100,6 @@ class Application(tk.Frame):
     def show_title_info(self,newWindow,counter,button_identity):
         button_name = (button_identity[counter])
         button_name.configure(bg="red")
-        
         info_text = ""
         
         for i in range(0,7787):
@@ -172,4 +145,3 @@ button_identities = []
 root = tk.Tk()
 app = Application(df, genre_list, button_identities, master=root)
 app.mainloop()
-
