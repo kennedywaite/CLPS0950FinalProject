e3aa60fe88b9eab4ef28ad75a5b812ed2d843561
wyuan4 <wen_yuan@brown.edu>
2021-04-18 20:04:42 -0500

    Update user_input.py
    adding some fun user inputs for potential user recommendation engine -- Ashley typing, Erik debugging (30 min)

--- user_input.py
+++ user_input.py
@@ -3,15 +3,24 @@
 import numpy as np
 
 user_matrix = np.zeros((4,7787))
+username_list = ['Ashley', 'Kennedy', 'Bryan', 'Erik']
 print(user_matrix)
 
-new_user = input("Are you a new user? [Yes/No]")
+new_user = input("Are you a new user? [Yes/No] ")
 
 if (new_user == "Yes"):
     #add new row to user_matrix
-    user_matrix.append((np.zeros(1,7787)))
+    user_matrix = np.vstack((user_matrix,(np.zeros((1,7787)))))
+    user_name = input("Input a username: ")
+    if isinstance(user_name, str) == True:
+        username_list.append(user_name)
+    else:
+        user_name = input("Input a username: ")
 elif (new_user == "No"):
-    user_matrix.append(1)
+    user_name = input("What is your user name? ")
     #ask for username
 else: 
-    new_user = input("Are you a new user? [Yes/No]")
+    new_user = input("Are you a new user? [Yes/No] ")
+    
+print(user_matrix)
+print(username_list)
